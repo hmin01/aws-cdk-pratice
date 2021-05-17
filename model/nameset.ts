48b21d30b00a1280b0b08608ed1e391004549219
hmin01 <goals1003@gmail.com>
2021-05-17 18:07:51 +0900

    2021.05.17. - Separate functions that create the role required for each aws resource - Add userdata in aws ec2 instance (script data that works at boot time)

---
+++ model/nameset.ts
@@ -0,0 +1,8 @@
+export const ROLE_LAMBDA             = 'privacyDAM-LambdaRole';
+export const ROLE_EC2                = 'privacyDAM-EC2Role';
+export const ROLE_APIGATEWAY         = 'privacyDAM-ApiGatewayRole';
+
+export const NAME_LAMBDA             = 'privacyDAM-Process';
+export const NAME_EC2                = 'privacyDAM-ManagementServer';
+export const NAME_EC2_SECURITY_GROUP = 'privacyDAM-BasicSecurityGroup'
+export const NAME_APIGATEWAY         = 'privacyDAM-APIs';
