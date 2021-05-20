f9228dcadae88a151a89750ac03e9c994921cb29
hmin01 <goals1003@gmail.com>
2021-05-20 17:55:01 +0900

    2021.05.20. - Add CloudFormation description

--- bin/aws-cdk-practice.ts
+++ bin/aws-cdk-practice.ts
@@ -16,6 +16,7 @@ new AwsCdkPracticeStack(app, 'privacyDAM', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
+  description: 'A solution called privacyDAM for controlling access to privacy data (by TOVDATA)',
   env: { account: '395824177941', region: 'ap-northeast-2' }
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
